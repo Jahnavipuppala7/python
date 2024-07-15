409aae25bd9dd16df58da09124b1a6513f074fa1
Jahnavipuppala7 <146073816+Jahnavipuppala7@users.noreply.github.com>
2024-07-15 05:58:51 +0530

    Update 46)lambda_fun.py

--- 46)lambda_args.py
+++ 46)lambda_args.py
@@ -1,7 +1,2 @@
-'''In lambda we use 3 operations to keep the code simple
-they are  map,filter and reduce'''
-list1=[3,7,4,5,62,1]
-lis1=list(filter(lambda x:x%2!=0,list1))
-print(lis1)
-lis2=list(map(lambda x:x*2,list1))
-print(lis2)
+x=lambda a:a*10
+print(x(5))
