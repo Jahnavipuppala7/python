6c6e0d5cffcfeaf6035a65dd61e78cc972a0dddb
Jahnavipuppala7 <146073816+Jahnavipuppala7@users.noreply.github.com>
2024-09-06 09:42:56 +0530

    Update 19)number_is_prime_or_not.py

--- 19)number_is_prime_or_not.py
+++ 19)number_is_prime_or_not.py
@@ -3,9 +3,7 @@ count=0
 for i in range(1,n+1):
   if n%i==0:
     count=count+1
-if count>2:
+if count>2 or count<2:
   print("not a prime")
-elif count==2:
-  print("prime")
 else:
-  print("not a prime")
+  print("prime")
